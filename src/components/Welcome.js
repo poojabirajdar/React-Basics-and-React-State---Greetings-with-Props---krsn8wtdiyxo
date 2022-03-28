9f8ad6118f2189975fadef4852979baeff19f897
Pooja Birajdar <37832058+poojabirajdar@users.noreply.github.com>
2022-03-28 19:22:04 +0530

    init

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+const Welcome = (props) => {
+  return (
+    <>
+      <h1>Hey {props.name}!</h1>
+      <section>
+        <h2>Welcome to Newton School.</h2>
+      </section>
+    </>
+  );
+};
+
+export default Welcome;
